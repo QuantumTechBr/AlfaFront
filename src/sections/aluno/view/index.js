7ba83b6b2e1787f7e429ce30304bc23881ac6588
Junior Kaibro <jrkaibro@gmail.com>
2023-09-20 07:43:40 -0300

    Update index.js

--- src/sections/aluno/view/index.js
+++ src/sections/aluno/view/index.js
@@ -1,5 +1,5 @@
 export { default as AlunoEditView } from './aluno-edit-view';
 export { default as AlunoListView } from './aluno-list-view';
-// export { default as AurmaCardsView } from './turma-cards-view';
+// export { default as AlunoCardsView } from './aluno-cards-view';
 export { default as AlunoCreateView } from './aluno-create-view';
-// export { default as AurmaProfileView } from './turma-profile-view';
+// export { default as AlunoProfileView } from './aluno-profile-view';
