656f0f27fa8fb22757f6b75840c0a7954e05f937
fabiombarreto <fabiobarreto208@gmail.com>
2023-09-21 01:36:05 -0300

    Alterando Titulo da página

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,9 +48,9 @@ import { AuthProvider, AuthConsumer } from 'src/auth/context/alfa';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimal UI Kit',
+  title: 'Alfa Project',
   description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
+    '',
   keywords: 'react,material,kit,application,dashboard,admin,template',
   themeColor: '#000000',
   manifest: '/manifest.json',
