adb8e991d946fc87c8ddb6b2f75236a27902e11d
guilhermeinovando <guilherme@inovando.online>
2023-10-06 00:03:49 -0300

    file manager funcional, faltando cors

--- src/sections/documento/file-manager-new-folder-dialog.js
+++ src/sections/documento/file-manager-new-folder-dialog.js
@@ -11,14 +11,51 @@ import Dialog from '@mui/material/Dialog';
 // components
 import Iconify from 'src/components/iconify';
 import { Upload } from 'src/components/upload';
-import { S3Client, CreateMultipartUploadCommand } from '@aws-sdk/client-s3';
 
 import { AWS_S3 } from 'src/config-global';
-import { insertDocumento, getAllDocumentos  } from './documento-repository';
+import { insertDocumento, getAllDocumentos } from './documento-repository';
+
+import { HttpRequest } from '@aws-sdk/protocol-http';
+import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
+import { parseUrl } from '@aws-sdk/url-parser';
+import { Sha256 } from '@aws-crypto/sha256-browser';
+import { Hash } from '@aws-sdk/hash-node';
+import { formatUrl } from '@aws-sdk/util-format-url';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { createRequest } from '@aws-sdk/util-create-request';
+import {
+  TranscribeClient,
+  StartTranscriptionJobCommand,
+} from "@aws-sdk/client-transcribe";
+
 // ----------------------------------------------------------------------
 
 // Credencial AWS-S3
-const s3Client = new S3Client({ region: AWS_S3.region, credentials: { accessKeyId: AWS_S3.accessKeyId, secretAccessKey: AWS_S3.secretAccessKey }});
+const s3Client = new S3Client({
+  region: AWS_S3.region,
+  credentials: { accessKeyId: AWS_S3.accessKeyId, secretAccessKey: AWS_S3.secretAccessKey },
+});
+
+// Create the AWS Transcribe transcription job.
+const createTranscriptionJob = async (recording, jobName, bucket, key) => {
+  // Set the parameters for transcriptions job
+  const params = {
+    TranscriptionJobName: jobName + '-job',
+    LanguageCode: 'pt-BR', // For example, 'en-US',
+    OutputBucketName: bucket,
+    OutputKey: key,
+    Media: {
+      MediaFileUri: recording, // For example, "https://transcribe-demo.s3-REGION.amazonaws.com/hello_world.wav"
+    },
+  };
+  try {
+    // Start the transcription job.
+    const data = await s3Client.send(new StartTranscriptionJobCommand(params));
+    console.log('Success - transcription submitted', data);
+  } catch (err) {
+    console.log('Error', err);
+  }
+};
 
 export default function FileManagerNewFolderDialog({
   title = 'Enviar arquivos',
@@ -32,7 +69,6 @@ export default function FileManagerNewFolderDialog({
   onChangeFolderName,
   ...other
 }) {
-
   const [files, setFiles] = useState([]);
 
   useEffect(() => {
@@ -55,46 +91,84 @@ export default function FileManagerNewFolderDialog({
   );
 
   const handleUpload = async () => {
-
     onClose();
     const file = files[0];
     const reader = new FileReader();
     reader.readAsArrayBuffer(file);
 
-    const params = {
-      Bucket: AWS_S3.bucketName,
-      Key: file.name,
-      Body: file,
-      ContentType: file.type,
-      ContentLength: file.size,
-    }
-    
-    let multipartUpload = new CreateMultipartUploadCommand(params);
+    // const params = {
+    //   Bucket: AWS_S3.bucketName,
+    //   Key: file.name,
+    //   Body: file,
+    //   ContentType: file.type,
+    //   ContentLength: file.size,
+    // }
+
+    // let multipartUpload = new CreateMultipartUploadCommand(params);
+
+    const Key = file.name;
+    let signedUrl;
+    let response;
+
+    //   const params = {
+    //     Bucket: AWS_S3.bucketName,
+    //     Key: file.name,
+    //     Body: file,
+    //     ContentType: file.type,
+    //     ContentLength: file.size,
+    //  }
 
     const novoDocumento = {
-      ano_id: 'e445d95b-e92c-4fe9-b6b2-10afc66178b9',  // Pegar o ano letivo current ver amanha com os meninos como pega o ano letivo current
-      destino: file.name,
-      link: AWS_S3.url + file.name,
+      ano_id: 'e445d95b-e92c-4fe9-b6b2-10afc66178b9', // Pegar o ano letivo current ver amanha com os meninos como pega o ano letivo current
+      destino: Key,
+      link: AWS_S3.url + Key,
+    };
+
+    try {
+      const signer = new S3RequestPresigner({ ...s3Client.config });
+
+      // creating file upload request
+      const request = await createRequest(
+        s3Client,
+        new PutObjectCommand({ Key, Bucket: AWS_S3.bucketName })
+      );
+
+      const expire = new Date(Date.now() + 60 * 60 * 1000);
+
+      const expiration = new Date(Date.now() + 60 * 60 * 1000);
+      // Create and format the presigned URL.
+      signedUrl = formatUrl(await signer.presign(request, expiration));
+      console.log(`\nPutting "${Key}"`);
+    } catch (err) {
+      console.log('Error creating presigned URL', err);
     }
 
     try {
-      const result = await s3Client.send(multipartUpload);
-      console.log('Upload successful:', result);
-    
-      try {
-        await insertDocumento(novoDocumento);
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
+      // Upload the object to the Amazon S3 bucket using a presigned URL.
+      response = await fetch(signedUrl, {
+        method: 'PUT',
+        headers: {
+          'content-type': file.type,
+        },
+        body: file.body,
+        
+      });
+      // Create the transcription job name. In this case, it's the current date and time.
+      const today = new Date();
+      const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      const time = today.getHours() + '-' + today.getMinutes() + '-' + today.getSeconds();
+      const jobName = date + '-time-' + time;
 
-    } catch (error) {
-      console.error('Upload error:', error);
-      throw error;
+      // Call the "createTranscriptionJob()" function.
+      createTranscriptionJob('s3://' + AWS_S3.bucketName + '/' + Key, jobName, AWS_S3.bucketName, Key);
+    } catch (err) {
+      console.log('Error uploading object', err);
     }
+
+    await insertDocumento(novoDocumento);
   };
-    
-    const handleRemoveFile = (inputFile) => {
+
+  const handleRemoveFile = (inputFile) => {
     const filtered = files.filter((file) => file !== inputFile);
     setFiles(filtered);
   };
