ef5f52ea688b6d0b8980faf49945589d3f219f69
fabiombarreto <fabiobarreto208@gmail.com>
2023-11-14 19:45:24 -0300

    Criando documento turma

---
+++ src/sections/documento-turma/file-manager-new-folder-dialog.js
@@ -0,0 +1,188 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState, useCallback } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+// @mui
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import Dialog from '@mui/material/Dialog';
+
+// components
+import Iconify from 'src/components/iconify';
+import { Upload } from 'src/components/upload';
+
+
+// utils
+import axios, { endpoints } from 'src/utils/axios';
+
+
+// hooks
+import { useBoolean } from 'src/hooks/use-boolean';
+
+
+import { AWS_S3 } from 'src/config-global';
+import { insertDocumento, getAllDocumentos } from './documento-turma-repository';
+
+import { HttpRequest } from '@aws-sdk/protocol-http';
+import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
+import { parseUrl } from '@aws-sdk/url-parser';
+import { Sha256 } from '@aws-crypto/sha256-browser';
+import { Hash } from '@aws-sdk/hash-node';
+import { formatUrl } from '@aws-sdk/util-format-url';
+import { createRequest } from '@aws-sdk/util-create-request';
+import {
+  TranscribeClient,
+  StartTranscriptionJobCommand,
+} from "@aws-sdk/client-transcribe";
+
+export default function FileManagerNewFolderDialog({
+  title = 'Enviar arquivos',
+  open,
+  onClose,
+  //
+  onCreate,
+  onUpdate,
+  //
+  folderName,
+  onChangeFolderName,
+  ...other
+}) {
+  const [files, setFiles] = useState([]);
+  
+  const uploading = useBoolean(false); 
+
+  useEffect(() => {
+    if (!open) {
+      setFiles([]);
+    }
+  }, [open]);
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const newFiles = acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      );
+
+      setFiles([...files, ...newFiles]);
+    },
+    [files]
+  );
+
+  const handleUpload = async () => {
+    const file = files[0];
+    let response;
+
+
+    const novoDocumento = {
+      ano_id: 'e445d95b-e92c-4fe9-b6b2-10afc66178b9', // Pegar o ano letivo atual
+      destino: "teste",
+      arquivo: file,
+    };
+    
+    try {
+
+      let formData = new FormData();
+
+      formData.append('ano_id', novoDocumento.ano_id);
+      formData.append('destino', novoDocumento.destino);
+      formData.append('arquivo', file)
+
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      }
+
+      const response = await insertDocumento(formData).catch(erro => {
+        console.log("upload erro");
+        logout();
+        throw erro;
+      });
+
+
+      onClose(response);
+      
+    } catch (err) {
+      console.log('Error uploading object', err);
+    } finally {
+      uploading.onFalse();
+    }
+    
+  };
+
+  const handleRemoveFile = (inputFile) => {
+    const filtered = files.filter((file) => file !== inputFile);
+    setFiles(filtered);
+  };
+
+  const handleRemoveAllFiles = () => {
+    setFiles([]);
+  };
+
+  return (
+    <Dialog fullWidth maxWidth="sm" open={open} onClose={onClose} {...other}>
+      <DialogTitle sx={{ p: (theme) => theme.spacing(3, 3, 2, 3) }}> {title} </DialogTitle>
+
+      <DialogContent dividers sx={{ pt: 1, pb: 0, border: 'none'}}>
+        {(onCreate || onUpdate) && (
+          <TextField
+            fullWidth
+            label="Nome da pasta"
+            value={folderName}
+            onChange={onChangeFolderName}
+            sx={{ mb: 3 }}
+          />
+        )}
+        {uploading.value ? 
+          <Box sx={{ display: 'flex', minHeight: '100px', alignItems: 'center', justifyContent: 'center', gap: '1em' }}>
+            <CircularProgress />
+            Enviando...
+          </Box> :
+          <Upload multiple files={files} onDrop={handleDrop} onRemove={handleRemoveFile} />
+        }
+
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          variant="contained"
+          startIcon={<Iconify icon="eva:cloud-upload-fill" />}
+          onClick={() => {uploading.onTrue(); handleUpload();}}
+        >
+          Subir documento
+        </Button>
+
+        {!!files.length && (
+          <Button variant="outlined" color="inherit" onClick={handleRemoveAllFiles}>
+            Remover todos
+          </Button>
+        )}
+
+        {(onCreate || onUpdate) && (
+          <Stack direction="row" justifyContent="flex-end" flexGrow={1}>
+            <Button variant="soft" onClick={onCreate || onUpdate}>
+              {onUpdate ? 'Save' : 'Create'}
+            </Button>
+          </Stack>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+FileManagerNewFolderDialog.propTypes = {
+  folderName: PropTypes.string,
+  onChangeFolderName: PropTypes.func,
+  onClose: PropTypes.func,
+  onCreate: PropTypes.func,
+  onUpdate: PropTypes.func,
+  open: PropTypes.bool,
+  title: PropTypes.string,
+};
---
+++ src/sections/documento-turma/view/file-manager-view.js
@@ -0,0 +1,374 @@
+'use client';
+
+import { useEffect, useState, useCallback, useContext } from 'react';
+// @mui
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+// utils
+import { fTimestamp } from 'src/utils/format-time';
+// _mock
+import { _allFiles, FILE_TYPE_OPTIONS } from 'src/_mock';
+// hooks
+import { useBoolean } from 'src/hooks/use-boolean';
+// components
+import Iconify from 'src/components/iconify';
+import EmptyContent from 'src/components/empty-content';
+import { fileFormat } from 'src/components/file-thumbnail';
+import { ConfirmDialog } from 'src/components/custom-dialog';
+import { useSettingsContext } from 'src/components/settings';
+import { useTable, getComparator } from 'src/components/table';
+//
+import FileManagerTable from '../file-manager-table';
+import FileManagerFilters from '../file-manager-filters';
+import FileManagerGridView from '../file-manager-grid-view';
+import FileManagerFiltersResult from '../file-manager-filters-result';
+import FileManagerNewFolderDialog from '../file-manager-new-folder-dialog';
+import documentoTurmaMethods from '../documento-turma-repository';
+import { Box, CircularProgress } from '@mui/material';
+import Alert from '@mui/material/Alert';
+
+// ----------------------------------------------------------------------
+
+const defaultFilters = {
+  name: '',
+  type: [],
+  startDate: null,
+  endDate: null,
+};
+
+// ----------------------------------------------------------------------
+
+export default function FileManagerView() {
+  const [errorMsg, setErrorMsg] = useState('');
+  const [warningMsg, setWarningMsg] = useState('');
+  const table = useTable({ defaultRowsPerPage: 10 });
+
+  const settings = useSettingsContext();
+
+  const openDateRange = useBoolean();
+
+  const confirm = useBoolean();
+
+  const upload = useBoolean();
+
+  const [view, setView] = useState('list');
+
+  const [documentos, setDocumentos] = useState([]);
+
+  const [tableData, setTableData] = useState([]);
+
+  const [filters, setFilters] = useState(defaultFilters);
+
+  const dateError =
+    filters.startDate && filters.endDate
+      ? filters.startDate.getTime() > filters.endDate.getTime()
+      : false;
+
+  const dataFiltered = applyFilter({
+    inputData: tableData,
+    comparator: getComparator(table.order, table.orderBy),
+    filters,
+    dateError,
+  });
+
+  const dataInPage = dataFiltered.slice(
+    table.page * table.rowsPerPage,
+    table.page * table.rowsPerPage + table.rowsPerPage
+  );
+
+  const canReset =
+    !!filters.name || !!filters.type.length || (!!filters.startDate && !!filters.endDate);
+
+  const notFound = (!dataFiltered.length && canReset) || !dataFiltered.length;
+
+
+  useEffect(() => {
+    buscaDocumentos({force:true}).then(retorno => {
+      if (retorno.length == 0) {
+        setWarningMsg('A API retornou uma lista vazia de documentos');
+      }
+      setTableData(retorno);
+    });
+  }, []);
+
+  const buscaDocumentos = async ({ force = false } = {}) => {
+    let returnData = documentos;
+    if (force || documentos.length == 0) {
+
+
+      const consultaAtual = documentoTurmaMethods.getAllDocumentos().then((response) => {
+        if (response.data == '' || response.data === undefined) response.data = [];
+
+        setDocumentos(response.data);
+        returnData = response.data;
+        return returnData;
+      }).catch((error) => {
+        setErrorMsg('Erro de comunicação com a API de documentos');
+      });
+
+      await consultaAtual.then((value) => {
+        returnData = value;
+      });
+    }
+
+    return returnData;
+  };
+
+  const handleChangeView = useCallback((event, newView) => {
+    if (newView !== null) {
+      setView(newView);
+    }
+  }, []);
+
+  const handleFilters = useCallback(
+    (name, value) => {
+      table.onResetPage();
+      setFilters((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    },
+    [table]
+  );
+
+  const handleDeleteItem = useCallback(
+    async (id) => {
+      const retorno = await documentoTurmaMethods.deleteDocumentoById(id).catch((error) => {
+        setErrorMsg('Erro de comunicação com a API de documentos no momento exclusão do documento');
+      });
+      if (retorno.status == 204) {
+
+        const remainingRows = tableData.filter((row) => row.id !== id);
+        setTableData(remainingRows);
+        
+        table.onUpdatePageDeleteRow(dataInPage.length);
+      } else {
+        console.log("Response status: ", retorno.status);
+        console.log("Erro: ", retorno.data);
+        throw new Error("Erro ao deletar arquivo: " + row.arquivo);
+      }
+    },
+    [dataInPage.length, table, tableData]
+  );
+
+  const handleDeleteItems = useCallback(() => {
+    const remainingRows = tableData.filter((row) => !table.selected.includes(row.id));
+    const deleteRows = tableData.filter((row) => table.selected.includes(row.id));
+
+    deleteRows.forEach(async row => {
+      const retorno = await documentoTurmaMethods.deleteDocumentoById(row.id).catch((error) => {
+        setErrorMsg('Erro de comunicação com a API de documentos no momento exclusão do documento');
+      });
+      if (retorno.status != 204) {
+        console.log("Response status: ", retorno.status);
+        console.log("Erro: ", retorno.data);
+        throw new Error("Erro ao deletar arquivo: " + row.arquivo);
+      }
+    });
+    setTableData(remainingRows);
+
+    table.onUpdatePageDeleteRows({
+      totalRows: tableData.length,
+      totalRowsInPage: dataInPage.length,
+      totalRowsFiltered: dataFiltered.length,
+    });
+  }, [dataFiltered.length, dataInPage.length, table, tableData]);
+
+  const handleResetFilters = useCallback(() => {
+    setFilters(defaultFilters);
+  }, []);
+
+  const handleUploadClose = useCallback((event) => {
+    console.log(event);
+    if(event?.data?.id) {
+      buscaDocumentos({force:true}).then(retorno => setTableData(retorno));
+    }
+    
+    upload.onFalse();
+  }, [])
+
+  const renderFilters = (
+    <Stack
+      spacing={2}
+      direction={{ xs: 'column', md: 'row' }}
+      alignItems={{ xs: 'flex-end', md: 'center' }}
+    >
+      <FileManagerFilters
+        openDateRange={openDateRange.value}
+        onCloseDateRange={openDateRange.onFalse}
+        onOpenDateRange={openDateRange.onTrue}
+        //
+        filters={filters}
+        onFilters={handleFilters}
+        //
+        dateError={dateError}
+        typeOptions={FILE_TYPE_OPTIONS}
+      />
+
+      <ToggleButtonGroup size="small" value={view} exclusive onChange={handleChangeView}>
+        <ToggleButton value="list">
+          <Iconify icon="solar:list-bold" />
+        </ToggleButton>
+
+        <ToggleButton value="grid">
+          <Iconify icon="mingcute:dot-grid-fill" />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </Stack>
+  );
+
+  const renderResults = (
+    <FileManagerFiltersResult
+      filters={filters}
+      onResetFilters={handleResetFilters}
+      //
+      canReset={canReset}
+      onFilters={handleFilters}
+      //
+      results={dataFiltered.length}
+    />
+  );
+
+  return (
+    <>
+      <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <Typography variant="h4">Documentos de Intervenção</Typography>
+          <Button
+            variant="contained"
+            startIcon={<Iconify icon="eva:cloud-upload-fill" />}
+            onClick={upload.onTrue}
+          >
+            Enviar 
+          </Button>
+        </Stack>
+
+        {!!errorMsg && <Alert severity="error">{errorMsg}</Alert>}
+        {!!warningMsg && <Alert severity="warning">{warningMsg}</Alert>}
+
+        <Stack
+          spacing={2.5}
+          sx={{
+            my: { xs: 3, md: 5 },
+          }}
+        >
+          {renderFilters}
+
+          {canReset && renderResults}
+        </Stack>
+
+        {notFound ? (
+                <Box sx={{
+                  height: 100,
+                  textAlign: "center",
+                }}>
+                  <Button
+                    disabled
+                    variant="outlined"
+                    startIcon={<CircularProgress />}
+                    sx={{
+                      bgcolor: "white",
+                    }}
+                  >
+                    Carregando
+                  </Button>
+                  
+                </Box>) : (
+          <>
+            {view === 'list' ? (
+              <FileManagerTable
+                table={table}
+                tableData={tableData}
+                dataFiltered={dataFiltered}
+                onDeleteRow={handleDeleteItem}
+                notFound={notFound}
+                onOpenConfirm={confirm.onTrue}
+              />
+            ) : (
+              <FileManagerGridView
+                table={table}
+                data={tableData}
+                dataFiltered={dataFiltered}
+                onDeleteItem={handleDeleteItem}
+                onOpenConfirm={confirm.onTrue}
+              />
+            )}
+          </>
+        )}
+      </Container>
+
+      <FileManagerNewFolderDialog open={upload.value} onClose={handleUploadClose} />
+
+      <ConfirmDialog
+        open={confirm.value}
+        onClose={confirm.onFalse}
+        title="Delete"
+        content={
+          <>
+            Tem certeza que deseja deletar permanentemente <strong> {table.selected.length} </strong> itens?
+          </>
+        }
+        action={
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              handleDeleteItems();
+              confirm.onFalse();
+            }}
+          >
+            Deletar
+          </Button>
+        }
+      />
+    </>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+function applyFilter({ inputData, comparator, filters, dateError }) {
+  const { name, type, startDate, endDate } = filters;
+
+  const stabilizedThis = inputData.map((el, index) => [el, index]);
+
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+
+  inputData = stabilizedThis.map((el) => el[0]);
+
+  if (name) {
+    inputData = inputData.filter(
+      (file) => {
+        if(file.nomeArquivo) {
+          return file.nomeArquivo.toLowerCase().indexOf(name.toLowerCase()) !== -1
+        } else {
+          return file.arquivo.toLowerCase().indexOf(name.toLowerCase()) !== -1
+        }
+      }
+    );
+  }
+
+  if (type.length) {
+    inputData = inputData.filter((file) => type.includes(fileFormat(file.type)));
+  }
+
+  if (!dateError) {
+    if (startDate && endDate) {
+      inputData = inputData.filter(
+        (file) =>
+          fTimestamp(file.createdAt) >= fTimestamp(startDate) &&
+          fTimestamp(file.createdAt) <= fTimestamp(endDate)
+      );
+    }
+  }
+
+  return inputData;
+}
