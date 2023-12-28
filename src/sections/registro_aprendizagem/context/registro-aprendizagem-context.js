9e8371775dc3c88ae93fadb2eef24b9d3c46d139
Haikukara <l.fiorini@hotmail.com>
2023-12-28 16:19:51 -0300

    avaliacao-fase-todas-regras-funcionais

--- src/sections/registro_aprendizagem/context/registro-aprendizagem-context.js
+++ src/sections/registro_aprendizagem/context/registro-aprendizagem-context.js
@@ -6,9 +6,10 @@ export const RegistroAprendizagemContext = createContext();
 
 export const RegistroAprendizagemProvider = ({ children }) => {
   const [registroAprendizagemFase, setRegistroAprendizagemFase] = useState([]);
-  let _consultaAtual;
+  const [registroAprendizagemFaseAlunoTurma, setRegistroAprendizagemFaseAlunoTurma] = useState([]);
 
   const buscaRegistroAprendizagemFaseByTurmaIdBimestreId = async ({ turmaId, bimestreId, force = false } = {}) => {
+    let _consultaAtual;
     let returnData = registroAprendizagemFase;
 
     if (force || registroAprendizagemFase?.length < 1 || registroAprendizagemFase[0]?.turma_id != turmaId || registroAprendizagemFase[0]?.bimestre.id != bimestreId){
@@ -39,8 +40,47 @@ export const RegistroAprendizagemProvider = ({ children }) => {
     return returnData;
   };
 
+  const buscaRegistroAprendizagemFaseByAlunoTurmaId = async ({ alunoTurmaId } = {}) => {
+    let returnData = registroAprendizagemFaseAlunoTurma;
+    let consulta;
+
+    if (registroAprendizagemFaseAlunoTurma?.length < 1 || registroAprendizagemFaseAlunoTurma[0].aluno_turma.id != alunoTurmaId){
+      if (!consulta) {
+        consulta = registroAprendizagemMethods.getAllRegistrosAprendizagemFase({
+          alunoTurmaId: alunoTurmaId,
+        }).then((response) => {
+          if (response?.data == '' || response?.data === undefined) response.data = [];
+          setRegistroAprendizagemFaseAlunoTurma(response.data);
+          returnData = response.data;
+          return returnData;
+          });
+        }
+        await consulta.then((value) => {
+          returnData = value;
+        });
+
+    }
+    return returnData;
+  };
+
+  const melhorResultadoAlunoTurma = async ({ alunoTurmaId }) => {
+    const mapResultados = {
+      'Não Avaliado' : 1,
+      'Pré Alfabética': 2,
+      'Alfabética Parcial': 3,
+      'Alfabética Completa': 4,
+      'Alfabética Consolidada': 5,
+    };
+    let resultados = await buscaRegistroAprendizagemFaseByAlunoTurmaId({alunoTurmaId: alunoTurmaId})
+    let melhor = 'Não Avaliado'
+    for (let index = 0; index < resultados.length; index++) {
+      melhor = mapResultados[melhor] < mapResultados[resultados[index].resultado] ? resultados[index].resultado : melhor
+    }
+    return melhor;
+  }
+
   return (
-    <RegistroAprendizagemContext.Provider value={{ registroAprendizagemFase, buscaRegistroAprendizagemFaseByTurmaIdBimestreId }}>
+    <RegistroAprendizagemContext.Provider value={{ registroAprendizagemFase, registroAprendizagemFaseAlunoTurma, buscaRegistroAprendizagemFaseByTurmaIdBimestreId, buscaRegistroAprendizagemFaseByAlunoTurmaId, melhorResultadoAlunoTurma }}>
       {children}
     </RegistroAprendizagemContext.Provider>
   );
