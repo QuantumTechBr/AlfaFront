9750520d31929138727099b87670c612b7fbd486
Haikukara <l.fiorini@hotmail.com>
2024-01-24 14:52:46 -0300

    Arrumando listando de professores

--- src/sections/plano_intervencao/plano-intervencao-new-edit-form.js
+++ src/sections/plano_intervencao/plano-intervencao-new-edit-form.js
@@ -146,11 +146,13 @@ export default function PlanoIntervencaoNewEditForm({ currentPlano, newFrom = fa
         preparado.onTrue(); 
       }
       profissionais.data.map((profissional) => {
-        let pro = {
-          label: profissional.profissional,
-          id: profissional.id,
+        if (profissional.funcao.nome == "PROFESSOR") {
+          let pro = {
+            label: profissional.profissional,
+            id: profissional.id,
+          }
+          lp.push(pro)
         }
-        lp.push(pro)
       });
       setListaProfissionais(lp);
     }).catch((error) => {
