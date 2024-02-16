4f872d5e6811eeea01f9c3f05b4e438c11272181
guilhermeinovando <guilherme@inovando.online>
2024-02-15 23:41:03 -0300

    ajust no carregamento inicial

--- src/sections/home/view/home-view.js
+++ src/sections/home/view/home-view.js
@@ -1,12 +1,19 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'src/routes/hook';
 
 // ----------------------------------------------------------------------
 
 export default function HomeView() {
   const router = useRouter();
-  router.replace('/dashboard');
+
+  useEffect(() => {
+    console.log('redirect to dashboard');
+    setTimeout(() => {
+      router.replace('/dashboard');
+    }, 150);
+  }, []);
 
   return <></>;
 }
