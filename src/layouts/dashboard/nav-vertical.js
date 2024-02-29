ad436a69341859878793f98f19fd9cbf776db3fb
guilhermeinovando <guilherme@inovando.online>
2024-02-29 19:36:38 -0300

    ajuste na altura do menu, scroll sobrando no bottom

--- src/layouts/dashboard/nav-vertical.js
+++ src/layouts/dashboard/nav-vertical.js
@@ -18,7 +18,7 @@ import { NavSectionVertical } from 'src/components/nav-section';
 //
 import { NAV } from '../config-layout';
 import { useNavData } from './config-navigation';
-import { NavToggleButton, NavUpgrade } from '../_common';
+import { NavToggleButton } from '../_common';
 
 // ----------------------------------------------------------------------
 
@@ -60,7 +60,6 @@ export default function NavVertical({ openNav, onCloseNav }) {
 
       <Box sx={{ flexGrow: 1 }} />
 
-      <NavUpgrade />
     </Scrollbar>
   );
 
