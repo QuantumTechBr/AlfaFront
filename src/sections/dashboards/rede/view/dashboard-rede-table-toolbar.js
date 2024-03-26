e6fc94acee5687adc47360c418cd319ba8e6eae2
guilhermeinovando <guilherme@inovando.online>
2024-03-26 05:43:19 -0300

    pne filter dashboard rede

--- src/sections/dashboards/rede/view/dashboard-rede-table-toolbar.js
+++ src/sections/dashboards/rede/view/dashboard-rede-table-toolbar.js
@@ -13,6 +13,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 // components
 import _ from 'lodash';
+import { necessidades_especiais } from 'src/_mock';
 
 // ----------------------------------------------------------------------
 
@@ -32,11 +33,14 @@ export default function DashboardRedeTableToolbar({
     [onFilters]
   );
 
+  const handleFilterPne = useCallback((event) => onFilters('pne', event.target.value), [onFilters]);
+  const handleFilterPneItem = useCallback((event) => onFilters('pneItem', event.target.value), [onFilters]);
+
   return (
     <Grid container spacing={2}>
       {anoLetivoOptions && anoLetivoOptions.length > 0 && (
         <Grid xs={6} md="auto">
-        <FormControl sx={{ width: {xs: "100%", md: 120 } }}>
+          <FormControl sx={{ width: { xs: '100%', md: 120 } }}>
             <InputLabel size="small">Ano Letivo</InputLabel>
             <Select
               size="small"
@@ -61,7 +65,7 @@ export default function DashboardRedeTableToolbar({
 
       {anoEscolarOptions && (
         <Grid xs={6} md="auto">
-          <FormControl sx={{ width: {xs: "100%", md: 120 } }}>
+          <FormControl sx={{ width: { xs: '100%', md: 120 } }}>
             <InputLabel size="small">Ano Escolar</InputLabel>
             <Select
               size="small"
@@ -88,6 +92,62 @@ export default function DashboardRedeTableToolbar({
                     checked={filters.anoEscolar.includes(option)}
                   />
                   {option}º ano
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      )}
+
+      <Grid xs={6} md="auto">
+        <FormControl sx={{ width: { xs: '100%', md: 180 } }}>
+          <InputLabel size="small">PNE</InputLabel>
+          <Select
+            size="small"
+            value={filters.pne}
+            onChange={handleFilterPne}
+            input={<OutlinedInput fullWidth label="Laudo PNE" />}
+            MenuProps={{
+              PaperProps: {
+                sx: { maxHeight: 240 },
+              },
+            }}
+          >
+            <MenuItem key={`pneOption_unset`} value={'-'}>
+              Todos os estudantes
+            </MenuItem>
+            <MenuItem key={`pneOption_semPne`} value={'false'}>
+              Sem necessidades especiais
+            </MenuItem>
+            <MenuItem key={`pneOption_comPne`} value={'true'}>
+              Com necessidades especiais
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+
+      {filters.pne === 'true' && (
+        <Grid xs={6} md="auto">
+          <FormControl sx={{ width: { xs: '100%', md: 180 } }}>
+            <InputLabel size="small">PNE</InputLabel>
+            <Select
+              size="small"
+              value={filters.pneItem}
+              onChange={handleFilterPneItem}
+              input={<OutlinedInput fullWidth label="Laudo PNE" />}
+              MenuProps={{
+                PaperProps: {
+                  sx: { maxHeight: 240 },
+                },
+              }}
+            >
+              <MenuItem key={`pneItemOption_unset`} value={'-'}>
+                Todas as necessidades
+              </MenuItem>
+
+              {necessidades_especiais?.slice(1).map((option) => (
+                <MenuItem key={`pneItemOption_${option}`} value={option}>
+                  {option}
                 </MenuItem>
               ))}
             </Select>
