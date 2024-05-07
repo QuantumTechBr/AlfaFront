c76c4ad1bd099d460b74ebf46b972f0b0110d7f3
Haikukara <l.fiorini@hotmail.com>
2024-05-07 09:48:29 -0300

    correcao paginacao acompanhamento, offset

--- src/sections/registro_aprendizagem/registro-aprendizagem-repository.js
+++ src/sections/registro_aprendizagem/registro-aprendizagem-repository.js
@@ -12,14 +12,14 @@ export const deleteRegistroAprendizagemByFilter = ({tipo='', bimestreId='', turm
 export const insertRegistroAprendizagemDiagnostico = payload => axios.post(endpoints.registro_aprendizagem.diagnostico.post, payload);
 export const getAllRegistrosAprendizagemDiagnostico = ({turmaId='', nome='', periodo='', promoAnoAnterior='', alunoTurmaId=''}) => axios.get(endpoints.registro_aprendizagem.diagnostico.list.concat(`/?turma=${turmaId}&nome=${nome}&periodo=${periodo}&promo_ano_anterior=${promoAnoAnterior}&aluno_turma=${alunoTurmaId}`));
 export const updateRegistroAprendizagemDiagnosticoById = (id, payload) => axios.patch(endpoints.registro_aprendizagem.diagnostico.update.concat(id), payload);
-export const getListIdTurmaRegistroAprendizagemDiagnostico = (payload) => axios.post(endpoints.registro_aprendizagem.diagnostico.idsTurmaList.concat(`/?limit=${payload.limit}`), payload);
+export const getListIdTurmaRegistroAprendizagemDiagnostico = (payload) => axios.post(endpoints.registro_aprendizagem.diagnostico.idsTurmaList.concat(`/?limit=${payload.limit}&offset=${payload.offset}`), payload);
 export const exportFileDiagnosticoList = (query) => axios.get(endpoints.registro_aprendizagem.diagnostico.list.concat(`/?`).concat(query));
 
 export const insertRegistroAprendizagemFase = payload => axios.post(endpoints.registro_aprendizagem.fase.post, payload);
 export const getAllRegistrosAprendizagemFase = ({turmaId='', bimestreId='', alunoTurmaId=''}) => axios.get(endpoints.registro_aprendizagem.fase.list.concat(`/?turma=${turmaId}&bimestre=${bimestreId}&aluno_turma=${alunoTurmaId}`));
 export const updateRegistroAprendizagemFaseById = (id, payload) => axios.patch(endpoints.registro_aprendizagem.fase.update.concat(id), payload);
 export const getRegistroAprendizagemFaseById = id => axios.get(endpoints.registro_aprendizagem.fase.get_by_id.concat(id));
-export const getListIdTurmaRegistroAprendizagemFase = (payload) => axios.post(endpoints.registro_aprendizagem.fase.idsTurmaList.concat(`/?limit=${payload.limit}`), payload);
+export const getListIdTurmaRegistroAprendizagemFase = (payload) => axios.post(endpoints.registro_aprendizagem.fase.idsTurmaList.concat(`/?limit=${payload.limit}&offset=${payload.offset}`), payload);
 export const exportFileFaseList = (query) => axios.get(endpoints.registro_aprendizagem.fase.list.concat(`/?`).concat(query));
 
 const registroAprendizagemMethods = {
