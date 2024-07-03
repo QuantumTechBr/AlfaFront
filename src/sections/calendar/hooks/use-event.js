0a2c7cdcca5494244f3b67b902808c3bb36f70b8
fabiombarreto <fabiobarreto208@gmail.com>
2024-07-03 00:55:06 -0300

    Verificando se usuário pode alterar o evento do calendario Atualizando calendario quando altera evento ou deleta evento

--- src/sections/calendar/hooks/use-event.js
+++ src/sections/calendar/hooks/use-event.js
@@ -1,12 +1,21 @@
 import { useMemo } from 'react';
+import { useContext } from 'react';
 import merge from 'lodash/merge';
 // _mock
 import { CALENDAR_COLOR_OPTIONS } from 'src/_mock/_calendar';
 
+import { AuthContext } from 'src/auth/context/alfa';
+
 // ----------------------------------------------------------------------
 
 export default function useEvent(events, selectEventId, selectedRange, openForm) {
+  
+  const { user } = useContext(AuthContext);
+  
   const currentEvent = events.find((event) => event.id === selectEventId);
+
+  let isAdmin = user?.permissao_usuario.find(permissaoUsuario => permissaoUsuario.nome.includes('ADMIN'));
+  let isEditavel = isAdmin ?? (currentEvent ? (currentEvent.criado_por?.id == user.id) : true)
 
   const defaultValues = useMemo(
     () => ({
@@ -29,7 +38,9 @@ export default function useEvent(events, selectEventId, selectedRange, openForm)
   }
 
   if (currentEvent || selectedRange) {
-    return merge({}, defaultValues, currentEvent);
+    let result = merge({}, defaultValues, currentEvent);
+    result.editavel = !!isEditavel;
+    return result;
   }
 
   return defaultValues;
