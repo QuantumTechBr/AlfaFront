988016d4649832ddf72481d4672cee0d9a44d5dc
Haikukara <l.fiorini@hotmail.com>
2024-09-28 17:50:33 -0300

    filtro de bismestre adicionado no context, bimestres nao iniciados nao irao ser opção

--- src/sections/bimestre/context/bimestre-context.js
+++ src/sections/bimestre/context/bimestre-context.js
@@ -7,14 +7,25 @@ export const BimestresProvider = ({ children }) => {
   const [bimestres, setBimestres] = useState([]);
   let _consultaAtual;
 
+  const filtrarBimestres = (todosBimestres) => {
+    const hoje = new Date();
+    const dataAtual = hoje.toISOString().slice(0, 10); // Formata a data atual para 'YYYY-MM-DD'
+  
+    return todosBimestres.filter(bim => {
+      const dataInicio = new Date(bim.data_inicio).toISOString().slice(0, 10);
+      return dataInicio < dataAtual;
+    });
+  }
+
   const buscaBimestres = async ({ force = false } = {}) => {
     let returnData = bimestres;
     if (force || bimestres.length == 0) {
       if (!_consultaAtual || force) {
         _consultaAtual = bimestreMethods.getAllBimestres().then((response) => {
           if (response.data == '' || response.data === undefined) response.data = [];
-          setBimestres(response.data);
-          returnData = response.data;
+          let bimestresFiltrados = filtrarBimestres(response.data);
+          setBimestres(bimestresFiltrados);
+          returnData = bimestresFiltrados;
           return returnData;
         });
       }
