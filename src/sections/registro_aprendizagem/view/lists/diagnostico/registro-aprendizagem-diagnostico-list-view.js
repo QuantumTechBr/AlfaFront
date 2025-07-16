451c4bf33d8984d0f71432d3d25460c4cdd7efad
fabiombarreto <fabiobarreto208@gmail.com>
2025-07-15 22:34:30 -0300

    Melhorando telas de importação

--- src/sections/registro_aprendizagem/view/lists/diagnostico/registro-aprendizagem-diagnostico-list-view.js
+++ src/sections/registro_aprendizagem/view/lists/diagnostico/registro-aprendizagem-diagnostico-list-view.js
@@ -412,7 +412,7 @@ export default function RegistroAprendizagemDiagnosticoListView() {
 
       setWarningMsg('Enviando arquivo. Por favor, aguarde... ' +
         'O processo de importação pode demorar alguns minutos e até mesmo horas dependendo do número de registros a serem validados. ' +
-        'Você pode acompanhar o status do processo pelo painel do Django Admin. Por favor, evite iniciar vários processos de importação simultaneamente. ' +
+        'Você pode acompanhar o status do processo pela página Importações. Por favor, evite iniciar vários processos de importação simultaneamente. ' +
         'Isso pode gerar lentidão no sistema e até mesmo inconsistência de dados.'
       );
 
