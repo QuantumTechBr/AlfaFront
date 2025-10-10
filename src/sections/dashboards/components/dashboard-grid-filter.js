289ec327d31b4077647b59a2030889a64bccdde5
fabiombarreto <fabiobarreto208@gmail.com>
2025-10-10 18:19:48 -0300

    Definindo filtro de  bimestre para o bimestre atual em dash de rede

--- src/sections/dashboards/components/dashboard-grid-filter.js
+++ src/sections/dashboards/components/dashboard-grid-filter.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { useCallback, useState } from 'react';
+import { use, useCallback, useState, useContext, useEffect } from 'react';
+import _ from 'lodash';
 
 // @mui
 import Stack from '@mui/material/Stack';
@@ -12,6 +13,8 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import FormControl from '@mui/material/FormControl';
 // components
 import Iconify from 'src/components/iconify';
+// contexts
+import { BimestresContext } from 'src/sections/bimestre/context/bimestre-context';
 // ----------------------------------------------------------------------
 
 export default function DashboardGridFilters({ filters, onFilters }) {
@@ -30,6 +33,38 @@ export default function DashboardGridFilters({ filters, onFilters }) {
   );
 
   const [bimestre, setBimestre] = useState('');
+  const { bimestres, buscaBimestres } = useContext(BimestresContext);
+
+  useEffect(() => {
+    buscaBimestres();
+  }, []); 
+
+  useEffect(() => {
+    if (filters.bimestre) {
+      setBimestre(filters.bimestre);
+    } else {
+      const hoje = new Date();
+      let bimestreAtual = _.first(bimestres);
+
+      bimestres?.forEach((bim) => {
+        let dataInicio = null;
+        let dataFim = null;
+        if(bim.data_inicio) {
+          dataInicio = new Date(bim.data_inicio);
+        }
+        if(bim.data_fim) {
+          dataFim = new Date(bim.data_fim);
+        }
+        if (hoje >= dataInicio && hoje <= dataFim && bim.ordinal > bimestreAtual.ordinal) {
+          bimestreAtual = bim;
+        }
+      });
+      if(bimestreAtual) {
+        setBimestre(bimestreAtual.ordinal);
+        onFilters('bimestre', bimestreAtual.ordinal);
+      }
+    }
+  }, [filters.bimestre, bimestres]);
 
 
   return (
@@ -72,9 +107,6 @@ export default function DashboardGridFilters({ filters, onFilters }) {
               onChange={(event) => handleBimestreFilter(event)}
               input={<OutlinedInput label="Bimestre" />}
             >
-              <MenuItem key='' value=''>
-                  Todos
-              </MenuItem>
               {[1, 2, 3, 4].map((i) => (
                 <MenuItem key={i} value={i}>
                   {i}º
